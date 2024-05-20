e91709fcdc52d79427e3f5691cd0804f028690e5
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-20 11:13:50 -0400

    Step 2

--- json-parser.cc
+++ json-parser.cc
@@ -55,6 +55,10 @@ int main(int argc, char** argv) {
             if (inkey == 2 && word[i] == '"') {
                inval++; 
             }
+            // if (inval >= 2 && word[i] == ',') {
+            //    cout << "Invalid comma after key value pair" << endl; 
+            //    return 1; 
+            // }
          }
          if (inval != 2 && inkey != 2) {
             cout << "Invalid key value pair" << endl; 
@@ -64,9 +68,45 @@ int main(int argc, char** argv) {
       }
       // In here, going to have to break it into lines and then run the above code 
       if (open == -1) {
+         // established that we have braces 
          if (word[0] == '{' && word[word.size() - 1] == '}') {
             open = 0; 
          }
+         for (int i = 0; i < word.size(); i++) {
+            if (word[i] == '"') {
+               if (!inkey && !inval) {
+                  inkey = 1; 
+               }
+               else if (inkey == 1) {
+                  inkey = 2; 
+               }
+               else if (inkey == 0 && inval == 1) {
+                  inval = 2; 
+                  //inkey = 0; 
+               }
+               else if (inval == 2) {
+                  inval = 0; 
+               }
+               else if (inval == 1) {
+                  cout << "Missing colon" << endl; 
+                  return 1; 
+               }
+            }
+            if (word[i] == ':' && inkey == 2) {
+               inval = 1; 
+               inkey = 0; 
+            }
+         //   cout << "inval: " << inval << " inkey: " << inkey << " letter: " << word[i] << endl; 
+         }
+         if (inval == 1) {
+            cout << "Invalid comma" << endl; 
+            return 1; 
+         }
+         if (inkey != 0) {
+            cout << "Invalid Key-value pair" << endl; 
+            return 1; 
+         }
+         
       }
       if (open != 0 && open != 1) {
          cout << "Invalid opening brace" << endl; 
@@ -75,6 +115,10 @@ int main(int argc, char** argv) {
    }
    if (open != 0) {
       cout << "Lacks opening or closing brace" << endl; 
+      return 1; 
+   }
+   if (word[word.size() - 2] == ',') {
+      cout << "Invalid closing comma" << endl; 
       return 1; 
    }
    return 0;
