25ce8f9c3b37a6c74770f9c6f62b871302f936f6
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-20 09:02:02 -0400

    Version 2: Step 1 complete

--- json-parser.cc
+++ json-parser.cc
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std; 
 int main(int argc, char** argv) {  
@@ -16,78 +17,33 @@ int main(int argc, char** argv) {
    ifstream input;  
    string filename = argv[1]; 
    input.open(filename); 
-   char character; 
-   int inobject = 0;
-   int opened = 0; 
-   int instring = 0; 
-   int key = 0; 
-   int value = 0; 
-   // 0: string, 1: numeric, 2: boolean, 3: numeric
-   int val_type = -1; 
-   while (input >> character) {
-      if (character == '{') {
-         opened = 1; 
-         inobject++; 
+   string word; 
+   int open = -1; 
+   // int inkey = -1; 
+   // int inval = -1; 
+   while (getline(input, word)) {
+      if (word == "{") {
+         if (open == 1) {
+            cout << "Invalid opening brace" << endl; 
+            return 1; 
+         }
+         open = 1; 
       }
-      if (character == '}') {
-         inobject--; 
-         if (inobject < 0) {
+      if (word == "}") {
+         if (!open) {
             cout << "Invalid closing brace" << endl; 
             return 1; 
          }
+         open = 0; 
       }
-      if (character == '"' && !instring) {
-         if (!key && !value) {
-            key = 1; 
-         }
-         instring = 1; 
-      }
-      else if (character == '"' && instring) {
-         instring = 0; 
-      }
-      if (isalpha(character) && !instring) {
-         cout << "Invalid file: character outside of a string" << endl; 
-         return 1; 
-      }
-      if (character == ':' && !instring && key) {
-         value = 1; 
-         key = 0; 
-      } 
-      else if (character == ':' && !instring) {
-         cout << "Invalid file: key, value pair" << endl; 
-         return 1; 
-      }
-      if (character == ',' && value && !instring) {
-         value = 0; 
-         key = 1; 
-         val_type = -1; 
-      } 
-      else if (character == ',' && !instring) {
-         cout << "Invalid file: value without a key" << endl; 
-         return 1; 
-      }
-      // set val_type
-      if (val_type == -1 && value) {
-         if (character == '"') {
-            val_type = 0; 
-         }
-         else if (character == 't' || 'f') {
-            val_type = 2; 
-         }
-         else if (character == 'n') {
-            val_type = 3;
-         }
-         else { 
-            val_type = 1; 
+      if (open == -1) {
+         if (word[0] == '{' && word[word.size() - 1] == '}') {
+            open = 0; 
          }
       }
    }
-   if (key) {
-      cout << "Invalid file: key opened but not completed" << endl; 
-      return 1; 
-   }
-   if (inobject != 0 || !opened) {
-      cout << "Invalid file: braces" << endl; 
+   if (open != 0) {
+      cout << "Lacks opening or closing brace" << endl; 
       return 1; 
    }
    return 0;
