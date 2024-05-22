865c15950f3a18c792cda3477ceca8d2a64c1491
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-22 07:08:32 -0400

    Step 2 rewrite complete

--- json-parser.cc
+++ json-parser.cc
@@ -22,6 +22,9 @@ int main(int argc, char** argv) {
    // That means, that we care that the number of things goes back to zero at the end. 
    int brackets = 0; 
    int first = 0; 
+   int key = 0; 
+   int val = 0; 
+   int quotes = 0; 
    while(input >> word) {
       if (word == '{') {
          brackets++; 
@@ -42,9 +45,44 @@ int main(int argc, char** argv) {
          cout << "Invalid curly braces" << endl; 
          return 1; 
       }
+      // Checking keys and values
+      if (key == 0 && word == '"' && val == 0) {
+         key++; 
+      }
+      else if (key == -1 && word == '"' && val == 0) {
+         key = 1; 
+      }
+      else if (key == 1 && word == '"') {
+         key = 0; 
+      }
+      if (word == ':' && key != 0) {
+         cout << "Invalid Key" << endl; 
+         return 1; 
+      }
+      else if (word == ':') {
+         val = 1; 
+      }
+      if (word == ',') {
+         val = 0; 
+         if (quotes != 0 && quotes != 2) {
+            cout << "Invalid string value" << endl; 
+            return 1; 
+         }
+         quotes = 0; 
+         key = -1; 
+      }
+      if (val) {
+         if (word == '"') {
+            quotes++; 
+         }
+      }
    }
    if (first == 0) {
       cout << "Empty input" << endl;
+      return 1; 
+   }
+   if (key == -1) {
+      cout << "Improper comma" << endl; 
       return 1; 
    }
    input.close(); 
