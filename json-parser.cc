fd1dde2de70c16ea1039ff15423b273f82bf3df9
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-23 08:33:25 -0400

    new stepup

--- json-parser.cc
+++ json-parser.cc
@@ -14,6 +14,47 @@ using namespace std;
 // Check each piece that it matches a token 
 // If it doesn't, throw an exception 
 
+string lexical_string(string& json) {
+   return json; 
+}
+string lexical_num(string& json) {
+   return json;
+}
+string lexical_bool(string& json) {
+   return json; 
+}
+
+string lexical_null(string& json) {
+   return json; 
+}
+// function that will break the json string into tokens
+int parse(string json) {
+   // going to cut string 
+   string temp_string; 
+   vector<string> tokens; 
+   while (json.length()) { 
+      temp_string = lexical_string(json); 
+      tokens.push_back(temp_string); 
+      temp_string = lexical_num(json); 
+      tokens.push_back(temp_string); 
+      temp_string = lexical_bool(json); 
+      tokens.push_back(temp_string); 
+      temp_string = lexical_null(json); 
+      tokens.push_back(temp_string); 
+      while (json[0] == ' ') {
+         json = json.substr(1); 
+      }
+      if (json[0] == '{' || json[0] == '}' || json[0] == '[' || json[0] == ']' || json[0] == ',' || json[0] == ':') {
+         temp_string = ""; 
+         temp_string+= json[0]; 
+         tokens.push_back(temp_string); 
+         json = json.substr(1); 
+      }
+      
+   }
+   return 0; 
+}
+
 int main(int argc, char** argv) {  
    // Input should contain a file
    if (argc < 2) {
@@ -28,7 +69,9 @@ int main(int argc, char** argv) {
    while(getline(input, temp)) {
       data+= temp; 
    }
-   cout << data << endl; 
+   int returnable = parse(data);
    input.close(); 
-   return 0;
+   // if returnable is 0, return 0
+   // else, have a series of error codes to return based upon what returnable is 
+   return returnable;
 }
